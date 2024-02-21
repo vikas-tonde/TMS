c4e3521db535a742a564148fe7a535c044a29ca9
Tonde <T50496@intelizign.com>
2024-02-21 10:12:32 +0530

    Project Structure created

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv"
+import bodyParser from "body-parser";
+import cors from 'cors';
+import mongoose from "mongoose";
+
+
+dotenv.config({ path: "dev.env" });
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("Database connected");
+});
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors({
+    credentials: true,
+    origin: "*"
+}));
+app.use(bodyParser.json())
+
+
+
+app.listen(port, () => console.log(`Server is runnning on port : http://localhost:${port}`));
